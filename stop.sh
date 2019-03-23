59bfa34eb3e75396be714838d25929e336003af1
favsol26 <favsol26@gmail.com>
2019-03-23 22:01:04 +0100

    setting docker up

---
+++ stop.sh
@@ -0,0 +1,15 @@
+BASE_SITE=do1819-02.com
+
+# development
+export NODE_ENV=development
+export PORT=8008
+export DBPORT=27018
+export VIRTUAL_HOST=$NODE_ENV.$BASE_SITE
+docker-compose -p $VIRTUAL_HOST down
+
+# production
+export NODE_ENV=production
+export PORT=8001
+export DBPORT=27011
+export VIRTUAL_HOST=$BASE_SITE
+docker-compose -p $VIRTUAL_HOST down 
