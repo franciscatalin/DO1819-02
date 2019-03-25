b7ed99c8cdc12737d570b24b09a870feef0142c4
favsol26 <favsol26@gmail.com>
2019-03-25 05:11:18 +0100

    fix auth

--- routerSevices.js
+++ routerSevices.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    admin = require('firebase-admin'),
-    serviceAccount = require('./acme-explorer-1176d-firebase-adminsdk-hpy9v-2636ee199c.json'),
+    // admin = require('firebase-admin'),
+    // serviceAccount = require('./acme-explorer-1176d-firebase-adminsdk-hpy9v-2636ee199c.json'),
     morgan = require('morgan'),
     cors = require('cors'),
     app = express();
@@ -11,10 +11,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://acme-explorer-1176d.firebaseio.com"
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://acme-explorer-1176d.firebaseio.com"
+// });
 
 var routesActors = require('./API/routes/actorsRoutes'),
     routesApplications = require('./API/routes/applicationsRoutes'),
