09019ab1b0a384ea7a7ef25985b97fa236d8a5e0
favsol26 <favsol26@gmail.com>
2019-03-29 05:13:43 +0100

    fixing mongo container

--- stop.sh
+++ stop.sh
@@ -2,14 +2,14 @@ BASE_SITE=do1819-02.com
 
 # development
 export NODE_ENV=development
-export PORT=8008
+export PORT=8078
 export DBPORT=27018
 export VIRTUAL_HOST=$NODE_ENV.$BASE_SITE
 docker-compose -p $VIRTUAL_HOST down
 
 # production
 export NODE_ENV=production
-export PORT=8001
+export PORT=8071
 export DBPORT=27011
 export VIRTUAL_HOST=$BASE_SITE
 docker-compose -p $VIRTUAL_HOST down 
